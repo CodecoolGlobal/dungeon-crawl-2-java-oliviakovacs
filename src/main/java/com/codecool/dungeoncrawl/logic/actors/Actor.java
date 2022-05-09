ae59c58512bac8848a282b0c2ba9086e91bdd161
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-05-09 16:26:23 +0200

    Add attack method to Actor class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -15,16 +15,43 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if(nextCell.getActor() == null && nextCell.getType() == CellType.FLOOR){
+        if(nextCell.getType() == CellType.FLOOR){
+            if (nextCell.getActor() == null) {
+                cell.setActor(null);
+                nextCell.setActor(this);
+                cell = nextCell;
+            } else {                               //...if there is a monster on the cell:
+                attack(nextCell);
+            }
+        }
+    }
+
+    public void attack(Cell nextCell) {
+        System.out.println("FIGHT!!!!");
+        Actor monster = nextCell.getActor();
+        monster.setHealth(monster.getHealth()-5);
+        if (monster.getHealth()>0) {                     // ...if the monster survived, it fights back:
+            System.out.println("Monster fights back!");
+            this.setHealth(this.getHealth()-2);
+            if (this.getHealth()<=0) {                   // ...if player dies:
+                System.out.println("You died! Game over.");
+            } else {                                     // ...if player survived:
+                System.out.println("Both survived.");
+            }
+        } else {                                         // ...if the monster died:
+            System.out.println("You killed the monster!!");
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
         }
-
     }
 
     public int getHealth() {
         return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
     }
 
     public Cell getCell() {
